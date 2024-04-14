5e5330fa81dfbc5ce7e1fec75dcf3fe583b299d6
Arthur G. <me@arthurka.eu>
2024-04-14 16:40:50 +0200

    feat: remove registering vendor step

--- contracts/DiscountCoupons.sol
+++ contracts/DiscountCoupons.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
-contract DiscountCoupons is ERC1155, Ownable, ERC1155Burnable, ERC1155Supply {
+contract DiscountCoupons is Ownable, ERC1155Supply, ERC1155Burnable {
   constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {}
 
   struct Vendor {
@@ -30,22 +30,19 @@ contract DiscountCoupons is ERC1155, Ownable, ERC1155Burnable, ERC1155Supply {
   mapping(uint couponCollectionID => CouponCollectionData) public couponCollections;
   uint public couponCollectionIDCounter;
 
-  function registerVendor(string memory name, string memory storeLink) external {
-    vendors[vendorIDCounter] = Vendor(msg.sender, name, storeLink);
-    vendorIDCounter++;
-  }
-
   function updateVendor(uint vendorID, string memory name, string memory storeLink) external {
     require(vendors[vendorID].vendorAddress == msg.sender, "Only vendor can update its data");
     vendors[vendorID].name = name;
     vendors[vendorID].storeLink = storeLink;
   }
 
-  function listCouponCollection(uint vendorID, uint256 discount, uint256 price, uint maxCouponAmount) external {
-    require(vendors[vendorID].vendorAddress == msg.sender, "Only vendor can list coupon collection");
+  function listCouponCollection(string memory name, string memory storeLink, uint vendorID, uint256 discount, uint256 price, uint maxCouponAmount) external {
+    vendors[vendorIDCounter] = Vendor(msg.sender, name, storeLink);
 
-    couponCollections[couponCollectionIDCounter] = CouponCollectionData(vendorID, discount, price, maxCouponAmount);
-    vendorCouponCollections[vendorID].push(couponCollectionIDCounter);
+    couponCollections[couponCollectionIDCounter] = CouponCollectionData(vendorIDCounter, discount, price, maxCouponAmount);
+    vendorCouponCollections[vendorIDCounter].push(couponCollectionIDCounter);
+    
+    vendorIDCounter++;
     couponCollectionIDCounter++;
   }
 
