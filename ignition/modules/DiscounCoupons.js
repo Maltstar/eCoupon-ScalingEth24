27288b68d95958c9700ac6749fb08d79c1c4a53b
Maltstar <hftool@proton.me>
2024-04-19 11:01:00 +0200

    original template saleor

--- ignition/modules/DiscounCoupons.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("DiscountCoupons", (m) => {
-  const lock = m.contract("DiscountCoupons", ["0x8f5c6660D5c43001325ba0489dF09C971de7c8f7"], {});
-
-  return { lock };
-});
